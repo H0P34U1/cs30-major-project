f5f205c3257d136146055cd01307b7c6267276cc
H0P34U1 <146876269+H0P34U1@users.noreply.github.com>
2023-12-20 11:59:22 -0600

    myLines

---
+++ myLines.js
@@ -0,0 +1,8 @@
+class myLines {
+  constructor(winMouseX, winMouseY, pwinMouseX, pwinMouseY) {
+    this.x = winMouseX;
+    this.y = winMouseY;
+    this.py = pwinMouseY;
+    this.px = pwinMouseX;
+  }
+}
