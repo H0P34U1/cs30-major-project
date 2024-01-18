9427c46913ed490ff7a7a3589686e51221776c82
H0P34U1 <146876269+H0P34U1@users.noreply.github.com>
2024-01-18 09:11:13 -0600

    small change

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-redeclare
 const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth - 60;
-canvas.height = 500;
+canvas.height = 400;
 
 let context = canvas.getContext("2d"); 
 let start_background_color = "white";
