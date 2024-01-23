ea1cc0bce4a7f797e76fb96488e6c3f25bfabbbe
H0P34U1 <146876269+H0P34U1@users.noreply.github.com>
2024-01-23 12:36:53 -0600

    more UI updates

--- reflection.md
+++ reflection.md
@@ -1,5 +1,6 @@
 what advice would i give myself?
 was everything in my need to have list completed
-the hardest part 
+the hardest part?
 was there any problems i couldnt solve?
+what am i most proud of?
 
--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 // Major project
 
 // eslint-disable-next-line no-redeclare
-const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth - 60;
-canvas.height = 450;
+const canvass = document.getElementById("canvass");
+canvass.width = window.innerWidth - 60;
+canvass.height = 500;
 
-let context = canvas.getContext("2d"); 
+let context = canvass.getContext("2d"); 
 let start_background_color = "white";
 context.fillStyle = start_background_color;
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.fillRect(0, 0, canvass.width, canvass.height);
 
 let draw_color = "black";
 let draw_width = "2";
@@ -22,26 +22,30 @@ function change_color(element) {
   draw_color = element.style.background;
 }
 
-canvas.addEventListener("touchstart", start, false);
-canvas.addEventListener("touchmove", draw, false);
-canvas.addEventListener("mousedown", start, false);
-canvas.addEventListener("mousemove", draw, false);
+function pencil() {
+  is_drawing = false;
+}
 
-canvas.addEventListener("touchend", stop, false);
-canvas.addEventListener("mouseup", stop, false);
-canvas.addEventListener("mouseout", stop, false);
+canvass.addEventListener("touchstart", start, false);
+canvass.addEventListener("touchmove", draw, false);
+canvass.addEventListener("mousedown", start, false);
+canvass.addEventListener("mousemove", draw, false);
+
+canvass.addEventListener("touchend", stop, false);
+canvass.addEventListener("mouseup", stop, false);
+canvass.addEventListener("mouseout", stop, false);
 
 
 function start(event) {
   is_drawing = true;
   context.beginPath();
-  context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+  context.moveTo(event.clientX - canvass.offsetLeft, event.clientY - canvass.offsetTop);
   event.preventDefault();
 }
 
 function draw(event) {
   if (is_drawing) {
-    context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    context.lineTo(event.clientX - canvass.offsetLeft, event.clientY - canvass.offsetTop);
     context.strokeStyle = draw_color;
     context.lineWidth = draw_width;
     context.lineCap = "round";
@@ -62,15 +66,15 @@ function stop(event) {
 
   // eslint-disable-next-line eqeqeq
   if ( event.type != "mouseout" ) {
-    restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    restore_array.push(context.getImageData(0, 0, canvass.width, canvass.height));
     index += 1;
   }
 }
 
 function clear_canvas() {
   context.fillStyle = start_background_color;
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvass.width, canvass.height);
+  context.fillRect(0, 0, canvass.width, canvass.height);
 
   restore_array = [];
   index = -1;
